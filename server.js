a2745d1867c58fa51bd9a6165560d06da9c85133
fuyunlong <1273179133@qq.com>
2022-08-20 07:56:04 +0800

    feat： 使用流形式读取文件

--- server.js
+++ server.js
@@ -16,9 +16,17 @@ const http = require('node:http')
 
 
 // 第二种 将html文件分离处理 使用fs进行读取
+// const fs = require('node:fs')
+// const html = fs.readFileSync('./index.html')
+// const server = http.createServer((req, res) => res.end(html))
+
+// 第三种 使用流
 const fs = require('node:fs')
 const html = fs.readFileSync('./index.html')
-const server = http.createServer((req, res) => res.end(html))
+const server = http.createServer((req, res) => {
+  // 此处需要手动处理下 Content-Length
+  fs.createReadStream('./index.html').pipe(res)
+})
 
 
 
