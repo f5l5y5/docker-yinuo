c57da1d04e2b1a4b84b019f68f93cd74d245cd23
fuyunlong <1273179133@qq.com>
2022-08-20 07:53:39 +0800

    feat： 文件读取html文件

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const http = require('node:http')
 
+//第一种
+// const html = `<!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8">
+//   <title></title>
+// </head>
+// <body>
+//   hello, shanyue. 
+// </body>
+// </html>`
+// const server = http.createServer((req, res) => res.end(html))
+
+
+
+// 第二种 将html文件分离处理 使用fs进行读取
 const fs = require('node:fs')
-
-
 const html = fs.readFileSync('./index.html')
+const server = http.createServer((req, res) => res.end(html))
 
-const server = http.createServer((req, res) => {
-    fs.createReadStream('./index.html').pipe(res)
-})
+
+
+
+// 
+
 server.listen(3000, () => {
   console.log('Listening 3000')
 })
